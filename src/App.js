0b8497f6617bbc968de13bbdedb62985999bd05f
Shawn Humphreys <shawnhum97@gmail.com>
2020-08-05 03:40:47 -0500

    ✨Read and open articles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
 import dotenv from 'dotenv'
 
+import wordsToNumbers from 'words-to-numbers'
+
 import {NewsCards} from './components/'
+import useStyles from './styles.js'
 
 dotenv.config()
 
@@ -11,13 +14,30 @@ const alanKey = process.env.REACT_APP_ALAN_KEY
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1)
+    const classes = useStyles()
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({command, articles}) => {
-                if( command === 'newHeadLines') {
+            onCommand: ({command, articles, number}) => {
+                if( command === 'newHeadlines') {
                     setNewsArticles(articles)
+                    setActiveArticle(-1)
+                } else if (command === 'highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1)
+                } else if(command === 'open'){
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if(parsedNumber > 20) {
+                        alanBtn().playText('Please try that again.')
+                    } else if(article) {
+                        window.open(article.url, '_blank')
+                        alanBtn().playText('Opening...')
+                    } else {
+                        alanBtn().playText('Please try that again...')
+                    }
                 }
             }
         })
