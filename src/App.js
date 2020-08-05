034d46bf87feba7a3cb8e520d455dcf102b1fb45
Shawn Humphreys <shawnhum97@gmail.com>
2020-08-04 19:26:55 -0500

    🔐Add Alan key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useEffect } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+const alanKey = process.env.REACT_APP_ALAN_KEY
 
 const App = () => {
     return (
