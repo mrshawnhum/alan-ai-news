66b42f8df0efbeb57973356dae9d103ee958006f
Shawn Humphreys <shawnhum97@gmail.com>
2020-08-04 23:29:35 -0500

    💄Style card components

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
 
-export const NewsCard = () => {
+import useStyles from './styles.js'
+
+export const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i }) => {
+    const classes = useStyles()
+    
     return (
-        <div>
-            News Card
-        </div>
+        <Card className={classes.card}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image={urlToImage || 'https://www.asanet.org/sites/default/files/default_images/placeholder-news.jpg'}/>
+                <div className={classes.details}>
+                    <Typography variant='body2' color='textSecondary' component='h2'>{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant='body2' color='textSecondary' component='h2'>{source.name}</Typography>
+                </div>
+                <Typography className={classes.title} gutterBottom variant='h5'>{title}</Typography>
+                <CardContent>
+                    <Typography variant='body2' color='textSecondary' component='p'>{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size='small' color='primary'>Learn More</Button>
+                <Typography variant='h5' color='textSecondary'>{i + 1}</Typography>
+            </CardActions>
+        </Card>
     )
 }
